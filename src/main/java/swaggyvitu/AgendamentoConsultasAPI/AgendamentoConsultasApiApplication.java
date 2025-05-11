31364c5ddbe2af7e03238dcee2632fda4e4fba1f
Victor Fernandes <victor.fernandes2004@gmail.com>
2025-05-10 23:14:47 -0400

    Iniciando projeto

---
+++ src/main/java/swaggyvitu/AgendamentoConsultasAPI/AgendamentoConsultasApiApplication.java
@@ -0,0 +1,13 @@
+package swaggyvitu.AgendamentoConsultasAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoConsultasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoConsultasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swaggyvitu/AgendamentoConsultasAPI/AgendamentoConsultasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package swaggyvitu.AgendamentoConsultasAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoConsultasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
